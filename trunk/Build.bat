d65f9fcc578c39ce741e8f324b7c97b016e733a0
pbevis <pbevis@4031c362-b1d6-11de-a03c-433460d76767>
2009-10-10 14:43:13 +0000

    Added build batch file and YUI compressor tool.

---
+++ trunk/Build.bat
@@ -0,0 +1,22 @@
+REM Define variables.
+SET version=1.0
+
+REM Create the build directory.
+IF EXIST Build RMDIR Build /S /Q
+MKDIR Build
+
+REM Version all the necessary files.
+COPY jquery.jqlog.js Build\jquery.jqlog-%version%.js
+COPY jqlog.levels.js Build\jqlog.levels-%version%.js
+COPY jqlog.alert.js Build\jqlog.alert-%version%.js
+COPY jqlog.console.js Build\jqlog.console-%version%.js
+
+REM Merge core, levels and console files to make a custom jqlog package. 
+COPY jquery.jqlog.js /B + jqlog.levels.js /B + jqlog.console.js /B Build\jquery.jqlog.custom-%version%.js
+
+REM Minify the files.
+java -jar yuicompressor-2.4.2.jar Build\jquery.jqlog-%version%.js -o Build\jquery.jqlog-%version%.min.js -v
+java -jar yuicompressor-2.4.2.jar Build\jqlog.levels-%version%.js -o Build\jqlog.levels-%version%.min.js -v
+java -jar yuicompressor-2.4.2.jar Build\jqlog.alert-%version%.js -o Build\jqlog.alert-%version%.min.js -v
+java -jar yuicompressor-2.4.2.jar Build\jqlog.console-%version%.js -o Build\jqlog.console-%version%.min.js -v
+java -jar yuicompressor-2.4.2.jar Build\jquery.jqlog.custom-%version%.js -o Build\jquery.jqlog.custom-%version%.min.js -v
