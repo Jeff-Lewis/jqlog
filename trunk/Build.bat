cb3680bcdbc282713a3f360b77e6c04c84ad9840
pbevis <pbevis@4031c362-b1d6-11de-a03c-433460d76767>
2009-10-17 19:00:10 +0000

    Renamed source files to make them less verbose.

--- trunk/Build.bat
+++ trunk/Build.bat
@@ -9,17 +9,17 @@ IF EXIST %build% RMDIR %build% /S /Q
 MKDIR %build%
 
 REM Version all the necessary files.
-CALL BatchSubstitute "X.X" "%version%" jquery.jqlog.js > %build%\jquery.jqlog-%version%.js
-CALL BatchSubstitute "X.X" "%version%" %plugins%\jqlog.levels.js > %build%\jqlog.levels-%version%.js
-CALL BatchSubstitute "X.X" "%version%" %targets%\jqlog.alert.js > %build%\jqlog.alert-%version%.js
-CALL BatchSubstitute "X.X" "%version%" %targets%\jqlog.console.js > %build%\jqlog.console-%version%.js
+CALL BatchSubstitute "X.X" "%version%" core.js > %build%\jquery.jqlog-%version%.js
+CALL BatchSubstitute "X.X" "%version%" %plugins%\levels.js > %build%\jqlog.levels-%version%.js
+CALL BatchSubstitute "X.X" "%version%" %targets%\alert.js > %build%\jqlog.target.alert-%version%.js
+CALL BatchSubstitute "X.X" "%version%" %targets%\console.js > %build%\jqlog.target.console-%version%.js
 
 REM Merge core, levels and console files to make a custom jqlog package. 
-COPY %build%\jquery.jqlog-%version%.js /B + %build%\jqlog.levels-%version%.js /B + %build%\jqlog.console-%version%.js /B %build%\jquery.jqlog-%version%.custom.js
+COPY %build%\jquery.jqlog-%version%.js /B + %build%\jqlog.levels-%version%.js /B + %build%\jqlog.target.console-%version%.js /B %build%\jquery.jqlog-%version%.custom.js
 
 REM Minify the files.
 java -jar yuicompressor-2.4.2.jar %build%\jquery.jqlog-%version%.js -o %build%\jquery.jqlog-%version%.min.js -v
 java -jar yuicompressor-2.4.2.jar %build%\jqlog.levels-%version%.js -o %build%\jqlog.levels-%version%.min.js -v
-java -jar yuicompressor-2.4.2.jar %build%\jqlog.alert-%version%.js -o %build%\jqlog.alert-%version%.min.js -v
-java -jar yuicompressor-2.4.2.jar %build%\jqlog.console-%version%.js -o %build%\jqlog.console-%version%.min.js -v
+java -jar yuicompressor-2.4.2.jar %build%\jqlog.target.alert-%version%.js -o %build%\jqlog.target.alert-%version%.min.js -v
+java -jar yuicompressor-2.4.2.jar %build%\jqlog.target.console-%version%.js -o %build%\jqlog.target.console-%version%.min.js -v
 java -jar yuicompressor-2.4.2.jar %build%\jquery.jqlog-%version%.custom.js -o %build%\jquery.jqlog-%version%.custom.min.js -v
