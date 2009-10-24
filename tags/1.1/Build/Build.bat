78e3f9e2d28eab602fe29a11e8844e80f49c9f07
pbevis <pbevis@4031c362-b1d6-11de-a03c-433460d76767>
2009-10-24 20:51:53 +0000

    Version 1.1

---
+++ tags/1.1/Build/Build.bat
@@ -0,0 +1,28 @@
+REM Define variables.
+SET version=1.1
+SET output=Release
+SET plugins=..\Plugins
+SET targets=..\Targets
+
+REM Create the output directory.
+IF EXIST %output% RMDIR %output% /S /Q
+MKDIR %output%
+
+REM Version all the necessary files.
+CALL BatchSubstitute "X.X" "%version%" "..\core.js" > "%output%\jquery.jqlog-%version%.js"
+CALL BatchSubstitute "X.X" "%version%" "%plugins%\levels.js" > "%output%\jqlog.levels-%version%.js"
+CALL BatchSubstitute "X.X" "%version%" "%plugins%\cookie.js" > "%output%\jqlog.cookie-%version%.js"
+CALL BatchSubstitute "X.X" "%version%" "%targets%\alert.js" > "%output%\jqlog.target.alert-%version%.js"
+CALL BatchSubstitute "X.X" "%version%" "%targets%\console.js" > "%output%\jqlog.target.console-%version%.js"
+CALL BatchSubstitute "X.X" "%version%" "ReadMe.txt" > "%output%\ReadMe.txt"
+
+REM Merge core, levels and console files to make a custom jqlog package. 
+COPY "%output%\jquery.jqlog-%version%.js" /B + "%output%\jqlog.levels-%version%.js" /B + "%output%\jqlog.cookie-%version%.js" /B + "%output%\jqlog.target.console-%version%.js" /B "%output%\jquery.jqlog-%version%.custom.js"
+
+REM Minify the files.
+ajaxmin "%output%\jquery.jqlog-%version%.js" /o "%output%\jquery.jqlog-%version%.min.js" /h
+ajaxmin "%output%\jqlog.levels-%version%.js" -o "%output%\jqlog.levels-%version%.min.js" /h
+ajaxmin "%output%\jqlog.cookie-%version%.js" -o "%output%\jqlog.cookie-%version%.min.js" /h
+ajaxmin "%output%\jqlog.target.alert-%version%.js" -o "%output%\jqlog.target.alert-%version%.min.js" /h
+ajaxmin "%output%\jqlog.target.console-%version%.js" -o "%output%\jqlog.target.console-%version%.min.js" /h
+ajaxmin "%output%\jquery.jqlog-%version%.custom.js" -o "%output%\jquery.jqlog-%version%.custom.min.js" /h
---
+++ tags/1.1/Targets/console.js
@@ -0,0 +1,68 @@
+/*
+ * jqlog.console X.X
+ *
+ * A log target for the jqlog framework that uses the console object.
+ *
+ * Copyright © 2009 Paul Bevis.
+ *
+ * Licensed under the MIT license
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * http://code.google.com/p/jqlog/
+ */ 
+jQuery.jqlog.targets.console = jQuery.extend({}, jQuery.jqlog.targetDefaults, {
+
+    /*
+    Target name.
+    */
+    name: "console",        
+    
+    /* 
+    Target version.
+    */
+    version: "X.X",
+    
+    /*
+    Logs a entry to the console if available.
+    
+    Parameters:
+       entry -   The entry to log.
+    */        
+    log: function log(entry) {
+        
+        var msg = entry.format();
+        
+        // Check for the browser console object...                
+        if (window.console) {
+            switch(entry.level) {
+                case "info":
+                    console.info(msg);
+                    break;
+                case "warn":
+                    console.warn(msg);
+                    break;
+                case "error":
+                    console.error(msg);
+                    break;
+                default:
+                    console.log(msg);
+            }
+        }
+        // Check for firebug lite...
+        else if (window.firebug) {
+            switch(entry.level) {
+                case "info":
+                    firebug.d.console.info(msg);
+                    break;
+                case "warn":
+                    firebug.d.console.warn(msg);
+                    break;
+                case "error":
+                    firebug.d.console.error(msg);
+                    break;
+                default:
+                    firebug.d.console.log(msg);
+            }
+        }
+    }
+});
