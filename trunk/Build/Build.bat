9ff43ff22e17e1e5180692ba5733047d5dda7497
pbevis <pbevis@4031c362-b1d6-11de-a03c-433460d76767>
2009-10-24 19:57:32 +0000

    Changed build process to use Microsoft's Ajax Minifier.

--- trunk/Build/Build.bat
+++ trunk/Build/Build.bat
@@ -1,6 +1,6 @@
 REM Define variables.
 SET version=1.1
-SET output=..\Release
+SET output=Release
 SET plugins=..\Plugins
 SET targets=..\Targets
 
@@ -19,9 +19,9 @@ REM Merge core, levels and console files to make a custom jqlog package.
 COPY "%output%\jquery.jqlog-%version%.js" /B + "%output%\jqlog.levels-%version%.js" /B + "%output%\jqlog.cookie-%version%.js" /B + "%output%\jqlog.target.console-%version%.js" /B "%output%\jquery.jqlog-%version%.custom.js"
 
 REM Minify the files.
-java -jar yuicompressor-2.4.2.jar "%output%\jquery.jqlog-%version%.js" -o "%output%\jquery.jqlog-%version%.min.js" -v
-java -jar yuicompressor-2.4.2.jar "%output%\jqlog.levels-%version%.js" -o "%output%\jqlog.levels-%version%.min.js" -v
-java -jar yuicompressor-2.4.2.jar "%output%\jqlog.cookie-%version%.js" -o "%output%\jqlog.cookie-%version%.min.js" -v
-java -jar yuicompressor-2.4.2.jar "%output%\jqlog.target.alert-%version%.js" -o "%output%\jqlog.target.alert-%version%.min.js" -v
-java -jar yuicompressor-2.4.2.jar "%output%\jqlog.target.console-%version%.js" -o "%output%\jqlog.target.console-%version%.min.js" -v
-java -jar yuicompressor-2.4.2.jar "%output%\jquery.jqlog-%version%.custom.js" -o "%output%\jquery.jqlog-%version%.custom.min.js" -v
+ajaxmin "%output%\jquery.jqlog-%version%.js" /o "%output%\jquery.jqlog-%version%.min.js" /h
+ajaxmin "%output%\jqlog.levels-%version%.js" -o "%output%\jqlog.levels-%version%.min.js" /h
+ajaxmin "%output%\jqlog.cookie-%version%.js" -o "%output%\jqlog.cookie-%version%.min.js" /h
+ajaxmin "%output%\jqlog.target.alert-%version%.js" -o "%output%\jqlog.target.alert-%version%.min.js" /h
+ajaxmin "%output%\jqlog.target.console-%version%.js" -o "%output%\jqlog.target.console-%version%.min.js" /h
+ajaxmin "%output%\jquery.jqlog-%version%.custom.js" -o "%output%\jquery.jqlog-%version%.custom.min.js" /h
