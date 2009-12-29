b313fed85ff8426cc2ede53c7e501e31969278ed
pbevis <pbevis@4031c362-b1d6-11de-a03c-433460d76767>
2009-12-29 19:43:01 +0000

    Version 1.2

---
+++ tags/1.2/Build/ReadMe.txt
@@ -0,0 +1,11 @@
+jqlog X.X
+
+jqlog is a logging framework plugin for jQuery that can be configured and extended allowing you to "roll your own" logging framework.  For more information visit http://code.google.com/p/jqlog/
+
+jquery.jqlog-X.X.js a combines the following scripts:
+
+jqlog.core-X.X.js
+jqlog.levels-X.X.js
+jqlog.target.console-X.X.js
+
+jqlog.cookie-X.X.js is dependant upon jquery.cookie.js (available from http://plugins.jquery.com/project/cookie).
---
+++ tags/1.2/Plugins/levels.js
@@ -0,0 +1,75 @@
+/*
+ * jqlog.levels X.X
+ *
+ * A logging level plugin for jqlog.
+ *
+ * Copyright © 2009 Paul Bevis.
+ *
+ * Licensed under the MIT license
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * http://code.google.com/p/jqlog/
+ */ 
+
+ /*
+Logs an infomation object with all registered log targets.
+
+Parameters:
+   object  -   The information object to be logged.
+   options -   Logging options passed to log targets
+
+Options:
+   level   -   Logging level.  Default value is "info".
+
+Usage: 
+   $.jqlog.info("Information");
+*/
+jQuery.jqlog.info = function(object, options) { 
+    var settings = jQuery.extend({
+        level: "info"
+    }, options);
+    this.log(object, settings);
+};
+        
+/*
+Logs a warning object with all registered log targets.
+
+Parameters:
+   object  -   The wanring object to be logged.
+   options -   Logging options passed to log targets
+
+Options:
+   level   -   Logging level.  Default value is "warn".
+
+Usage: 
+   $.jqlog.warn("Warning");
+*/         
+jQuery.jqlog.warn = function(object, options) {    
+    var settings = jQuery.extend({
+        level: "warn"
+    }, options);
+    this.log(object, settings);
+};
+
+/*
+Logs an error object with all registered log targets.
+
+Parameters:
+   object  -   The error object to be logged.
+   options -   Logging options passed to log targets
+
+Options:
+   level   -   Logging level.  Default value is "error".
+
+Usage: 
+   $.jqlog.error("Error");
+*/         
+jQuery.jqlog.error = function(object, options) {    
+    var settings = jQuery.extend({
+        level: "error"
+    }, options);
+    this.log(object, settings);
+};
+
+// Extend the log entry defaults object to include a default log level.
+jQuery.jqlog.entryDefaults.level = "debug";
